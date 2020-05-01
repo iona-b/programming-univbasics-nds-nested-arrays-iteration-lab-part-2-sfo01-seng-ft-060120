ec0762ef62d64152e2a424a1ab2d8bc1054d184b
Iona <ionabrabender@gmail.com>
2020-05-01 22:23:32 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,9 +1,12 @@
+require 'pry'
+
 def find_min_in_nested_arrays(src)
   min_temps = []
   row_count = 0 
   while row_count < src.length do
     index_count = 0
     min_temp = src[row_count][index_count]
+    binding.pry
     while index_count < src.length[row_count] do
       if min_temp < src[row_count][index_count]
         min_temp = src[row_count][index_count]
